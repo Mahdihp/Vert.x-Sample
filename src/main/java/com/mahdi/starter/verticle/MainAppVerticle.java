57c84f3b487637a20e79b8b5472b475d92b47de8
mahdihp <mahdi91151122526>
2020-10-15 11:50:09 +0330

    --init git  1399-7-24

--- src/main/java/com/mahdi/starter/verticle/MainAppVerticle.java
+++ src/main/java/com/mahdi/starter/verticle/MainAppVerticle.java
@@ -16,7 +16,6 @@ import org.slf4j.LoggerFactory;
 
 public class MainAppVerticle extends RestfulApiVerticle {
 
-
   private static final String HOST = "0.0.0.0";
   private static final int PORT = 8000;
   private SystemInfoController systemInfoController;
